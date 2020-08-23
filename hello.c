f0c14bb52b5bb8318770aea46509f8130039200a
Mpiva155 <69861291+Mpiva155@users.noreply.github.com>
2020-08-23 13:08:48 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Mason Piva
+ * Date: 2020
+ *
+ * A simple hello world program
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Mason Piva\n");
+    printf("Computer Engineering\n");
+
+
+
+ }
